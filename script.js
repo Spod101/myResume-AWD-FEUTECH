9ef10b2b4048ecf5dc9ee00fd5e0029474dfe1a7
Dalet <104555136+Spod101@users.noreply.github.com>
2024-01-12 21:39:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button');
+const element = document.querySelector('.resume');
+
+button.addEventListener('click', () => {
+    if (element.style.backgroundColor === 'darkgreen') {
+        element.style.color = '';
+        element.style.backgroundColor = '';
+        element.style.boxShadow = '';
+        document.querySelector('hr').style.borderColor = '';
+        document.querySelector('body').style.backgroundColor = '';
+    } else {
+        element.style.color = 'white';
+        element.style.backgroundColor = 'darkgreen';
+        element.style.boxShadow = '0 0 100px darkgreen';
+        document.querySelector('hr').style.borderColor = 'white';
+        document.querySelector('body').style.backgroundColor = '#252526';
+    }
+    element.style.transition = 'all 0.3s ease-in-out';
+    document.querySelector('hr').style.transition = 'border-color 0.3s ease-in-out';
+    document.querySelector('body').style.transition = 'background-color 0.3s ease-in-out';
+});
